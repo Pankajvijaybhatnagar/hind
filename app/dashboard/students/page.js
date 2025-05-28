8cd115435c5078d450817384b5b442ee42630ce4
ankitjnvynr <ankitbkana@outlook.com>
2025-05-28 20:29:47 +0530

    feat: update API base URL and improve student data handling in dashboard

--- app/dashboard/students/page.js
+++ app/dashboard/students/page.js
@@ -10,67 +10,66 @@ import React, { useEffect, useState } from "react";
 const page = () => {
   const [islistShow, setIsListShow] = React.useState(true);
   const [studentData, setStudentData] = useState({
-    name: '',
-    fatherName: '',
-    motherName: '',
-    dateOfBirth: '',
-    aadharCardNumber: '',
-    enrolmentNumber: '',
-    enrolmentDate: '',
-    courseName: '',
-    courseStatus: '',
-    academicDivision: '',
-    courseDuration: '',
-    totalObtainedMarks: '',
-    overallPercentage: '',
-    grade: '',
-    finalResult: '',
-    certificateIssueDate: '',
-    trainingCentre: '',
+    name: "",
+    fatherName: "",
+    motherName: "",
+    dateOfBirth: "",
+    aadharCardNumber: "",
+    enrolmentNumber: "",
+    enrolmentDate: "",
+    courseName: "",
+    courseStatus: "",
+    academicDivision: "",
+    courseDuration: "",
+    totalObtainedMarks: "",
+    overallPercentage: "",
+    grade: "",
+    finalResult: "",
+    certificateIssueDate: "",
+    trainingCentre: "",
     avatar: null,
   });
-
 
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
-  const [totalRecords, setTotalRecords] = useState(0)
-  const router = useRouter()
+  const [totalRecords, setTotalRecords] = useState(0);
+  const router = useRouter();
   const [filters, setFilters] = useState({
-    name: '',
-    enrolmentNo: '',
-    courseName: '',
-    courseStatus: '',
-    academicDivision: '',
-   
+    name: "",
+    enrolmentNo: "",
+    courseName: "",
+    courseStatus: "",
+    academicDivision: "",
+
     pageSize: 10,
   });
   const [students, setStudents] = useState([]);
 
   const handleAddNewStudent = () => {
     setStudentData({
-      name: '',
-      fatherName: '',
-      motherName: '',
-      dateOfBirth: '',
-      aadharCardNumber: '',
-      enrolmentNumber: '',
-      enrolmentDate: '',
-      courseName: '',
-      courseStatus: '',
-      academicDivision: '',
-      courseDuration: '',
-      totalObtainedMarks: '',
-      overallPercentage: '',
-      grade: '',
-      finalResult: '',
-      certificateIssueDate: '',
-      trainingCentre: '',
+      name: "",
+      fatherName: "",
+      motherName: "",
+      dateOfBirth: "",
+      aadharCardNumber: "",
+      enrolmentNumber: "",
+      enrolmentDate: "",
+      courseName: "",
+      courseStatus: "",
+      academicDivision: "",
+      courseDuration: "",
+      totalObtainedMarks: "",
+      overallPercentage: "",
+      grade: "",
+      finalResult: "",
+      certificateIssueDate: "",
+      trainingCentre: "",
       avatar: null,
-    })
-    setIsListShow(false)
-  }
+    });
+    setIsListShow(false);
+  };
 
   const handleListShow = () => {
     setIsListShow(!islistShow);
@@ -79,8 +78,7 @@ const page = () => {
   const handleEditStudent = (student) => {
     setStudentData(student);
     setIsListShow(false);
-    
-  }
+  };
 
   const getStudents = async (page) => {
     try {
@@ -88,34 +86,35 @@ const page = () => {
       const query = new URLSearchParams({
         ...filters,
       }).toString();
-      const response = await fetch(`${conf.apiBaseUri}/certificates?page=${currentPage}&${query}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": `Bearer ${localStorage.getItem("token")}`, // Assuming you store token in localStorage
-          
-        },
-      });
+      const response = await fetch(
+        `${conf.apiBaseUri}/certificates?page=${currentPage}&${query}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`, // Assuming you store token in localStorage
+          },
+        }
+      );
       const data = await response.json();
-      console.log(data)
-      if (data.error ) {
-        localStorage.removeItem("token")
-        router.push("/login")
-        return
-        
+      console.log(data);
+      if (data.error) {
+        localStorage.removeItem("token");
+        router.push("/login");
+        return;
       }
-      
+
       setStudents(data?.data);
       setTotalPages(data?.pagination?.totalPages);
-      setTotalRecords(data?.pagination?.totalRecords)
+      setTotalRecords(data?.pagination?.totalRecords);
     } catch (error) {
       console.log("Error fetching students:", error);
     }
-  }
+  };
 
   useEffect(() => {
     getStudents();
-  }, [filters,currentPage]);
+  }, [filters, currentPage]);
 
   return (
     <>
@@ -123,38 +122,92 @@ const page = () => {
       {islistShow ? (
         <div>
           <div className="d-flex justify-content-between align-items-center mb-1">
-            
-            <div className="d-flex gap-1" >
-              <input onChange={(e)=>setFilters({...filters,name:e.target.value})} className="form-control form-control-sm my-0 py-1" placeholder="Name" />
-              <input onChange={(e)=>setFilters({...filters,fatherName:e.target.value})} className="form-control form-control-sm my-0 py-1" placeholder="Father Name" />
-              <input onChange={(e)=>setFilters({...filters,aadharCardNumber:e.target.value})} className="form-control form-control-sm my-0 py-1" placeholder="Aadhar" />
-              <input onChange={(e)=>setFilters({...filters,enrolmentNumber:e.target.value})} className="form-control form-control-sm my-0 py-1" placeholder="Enrollment" />
-              <input onChange={(e)=>setFilters({...filters,courseName:e.target.value})} className="form-control form-control-sm my-0 py-1" placeholder="Course" />
+            <div className="d-flex gap-1">
+              <input
+                onChange={(e) =>
+                  setFilters({ ...filters, name: e.target.value })
+                }
+                className="form-control form-control-sm my-0 py-1"
+                placeholder="Name"
+              />
+              <input
+                onChange={(e) =>
+                  setFilters({ ...filters, fatherName: e.target.value })
+                }
+                className="form-control form-control-sm my-0 py-1"
+                placeholder="Father Name"
+              />
+              <input
+                onChange={(e) =>
+                  setFilters({ ...filters, aadharCardNumber: e.target.value })
+                }
+                className="form-control form-control-sm my-0 py-1"
+                placeholder="Aadhar"
+              />
+              <input
+                onChange={(e) =>
+                  setFilters({ ...filters, enrolmentNumber: e.target.value })
+                }
+                className="form-control form-control-sm my-0 py-1"
+                placeholder="Enrollment"
+              />
+              <input
+                onChange={(e) =>
+                  setFilters({ ...filters, courseName: e.target.value })
+                }
+                className="form-control form-control-sm my-0 py-1"
+                placeholder="Course"
+              />
             </div>
-            <button onClick={handleAddNewStudent} className="btn btn-primary btn-sm">
+            <button
+              onClick={handleAddNewStudent}
+              className="btn btn-primary btn-sm"
+            >
               Add new
             </button>
           </div>
-          <StudentsList totalPages={totalPages} students={students} handleEditStudent={handleEditStudent} />
-          <span className="d-flex justify-content-between align-items-center">
-          <Pagination
-            currentPage={currentPage}
-            totalPages={totalPages}
-            setCurrentPage={setCurrentPage}
-          />
-          <span>Showing {students.length}/{totalRecords} students</span>
-          </span>
+          {studentData.length > 0 ? (
+            <>
+              <StudentsList
+                totalPages={totalPages}
+                students={students}
+                handleEditStudent={handleEditStudent}
+              />
+              <span className="d-flex justify-content-between align-items-center">
+                <Pagination
+                  currentPage={currentPage}
+                  totalPages={totalPages}
+                  setCurrentPage={setCurrentPage}
+                />
+                <span>
+                  Showing {students.length}/{totalRecords} students
+                </span>
+              </span>
+            </> ): (
+            <div className="text-center mt-5">
+                <h4>No student found</h4>
+                <button
+              onClick={handleAddNewStudent}
+              className="btn btn-outline-primary btn-sm"
+            >
+              Add new
+            </button>
+            </div>
+          )}
         </div>
       ) : (
-          <>
-            <div className="d-flex justify-content-between align-items-center mb-3">
+        <>
+          <div className="d-flex justify-content-between align-items-center mb-3">
             <span className="fs-6 fw-bolder">Adding New Student</span>
-            
-          <button onClick={handleListShow} className="btn btn-primary">
-            All students
-          </button>
+
+            <button onClick={handleListShow} className="btn btn-primary">
+              All students
+            </button>
           </div>
-          <AddStudentForm studentData={studentData} setStudentData={setStudentData} />
+          <AddStudentForm
+            studentData={studentData}
+            setStudentData={setStudentData}
+          />
         </>
       )}
     </>
