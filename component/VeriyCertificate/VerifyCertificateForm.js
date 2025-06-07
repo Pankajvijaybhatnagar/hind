4f6bb9ad5bd524fa76fc31777c416b6c1038af1d
ankitjnvynr <ankitbkana@outlook.com>
2025-06-07 12:08:31 +0530

    added the roll no option at verify

--- component/VeriyCertificate/VerifyCertificateForm.js
+++ component/VeriyCertificate/VerifyCertificateForm.js
@@ -13,14 +13,14 @@ const VerifyCertificateForm = ({certificateId,setCertificateId,getCertificate}) 
     };      
     return (
       <div className="card">
-        <h5 className="card-header">Enter Your certificate id to verify</h5>
+        <h5 className="card-header">Enter Your Roll No <span className='text-danger fw-bold'>OR</span> certificate id to verify</h5>
         <div className="card-body">
           
           <form className="input-group" onSubmit={handleSubmit}>
             <input
               type="text"
               className="form-control"
-              placeholder="Enter certificate ID"
+              placeholder="Enter Roll no OR  certificate ID"
               aria-label="Recipient's username"
               aria-describedby="button-addon2"
               value={certificateId}
